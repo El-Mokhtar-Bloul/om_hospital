2707ad3f4d9cfbc9b379fc239ddaf42747ff695d
BLOUL <lweli.tt@gmail.com>
2023-08-15 09:38:38 +0000

    [ADD] Ajout du module om_hospital

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Hospital Manegement System',
+    'author': 'Mohamedhen Mokhtar Bloul',
+    'website': 'www.odoomates.tech',
+    'summary': 'Odoo16 Developement',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient.xml',
+    ]
+}
